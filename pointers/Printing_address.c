6d544ce8682687de8607ee02e34e1e9abc3ae5ee
Shubham Singh <73664329+shubhamsingh77ss@users.noreply.github.com>
2025-08-19 14:27:37 +0530

    feat:add program to demonstrate printing addresses of variables using pointers in C
    Created a program named pointer_addresses.c.
    
    Declares integer, float, and char variables.
    
    Assigns their addresses to corresponding pointers.
    
    Prints addresses stored in pointers using printf.
    
    Serves as a basic introduction to pointer concepts in C.

---
+++ pointers/Printing_address.c
@@ -0,0 +1,29 @@
+//program to demonstrate printing addresses of variables using pointers in C
+
+#include<stdio.h>
+
+int main()
+{
+    // Declare an integer variable and a pointer to int
+    int *pt;
+    int a = 100;
+
+    // Declare a float variable and a pointer to float
+    float *fp;
+    float b = 90.99;
+
+    // Declare a char variable and a pointer to char
+    char *cp;
+    char bool = 'Y';
+
+    // Assign addresses of variables to their respective pointers
+    pt = &a;
+    fp = &b;
+    cp = &bool;
+
+    // Print the memory addresses stored in the pointers
+    // (Note: %u is used here as in original code, though %p is recommended)
+    printf("Address of a = %u\n", pt);
+    printf("Address of b = %u\n", fp);
+    printf("Address of bool = %u\n", cp);
+}
