d64d3233d888dff6e779fa581dc844b5f5992efa
Shubham Singh <73664329+shubhamsingh77ss@users.noreply.github.com>
2025-08-27 10:53:39 +0530

    feat: add program for passing structure to a function using call by reference

---
+++ revision/structure7.c
@@ -0,0 +1,24 @@
+//write a program for passing structure to a function using call by reference
+#include<stdio.h>
+struct employees
+{
+    unsigned int emp_code;
+    int basic_sal;
+    float hra,pf,bonus;
+};
+int main()
+{
+    struct employees evar;
+    float get_netsalary(struct employees *);//prototype
+    float net_sal;
+    printf("Enter the employee code, basic salary, hra, pf and bonus\n");
+    scanf("%u%d%f%f%f",&evar.emp_code,&evar.basic_sal,&evar.hra,&evar.pf,&evar.bonus);
+    net_sal=get_netsalary(&evar);//calling function by refrence
+    printf("The Net salary=%8.2f",net_sal);
+}
+float get_netsalary(struct employees *fvar)
+{
+    float net_sal;
+    net_sal=fvar->basic_sal+fvar->hra+fvar->bonus-fvar->pf;
+    return net_sal;
+}
